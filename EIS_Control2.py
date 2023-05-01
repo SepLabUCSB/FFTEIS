f89bf0b06053c240e1d1baa3df89d0edc084d3a9
Brian Roehrich <bwroehrich@gmail.com>
2023-05-01 12:41:24 -0700

    work on gui

--- EIS_Control2.py
+++ EIS_Control2.py
@@ -9,7 +9,9 @@ from tkinter.ttk import *
 # Requirements
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 # Local modules
 # from modules import *
@@ -74,17 +76,69 @@ class MasterModule():
             if hasattr(module, 'stop'):
                 module.stop()
 
-        
+ 
+class PrintLogger(): 
+    '''
+    File like object to print console output into Tkinter window
+    set sys.stdout = PrintLogger, then print() will print to
+    PrintLogger.textbox
+    '''
+    def __init__(self, textbox): 
+        self.textbox = textbox # tk.Text object
+
+    def write(self, text):
+        self.textbox.insert(END, text) # write text to textbox
+        self.textbox.see('end') # scroll to end
+
+    def flush(self): # needed for file like object
+        pass
+       
     
     
 class GUI():
     
-    def __init__(self, master):
+    def __init__(self, root, master):
         self.willStop = False
         self.master = master
         self.master.register(self)
         
-        # Make all GUI windows
+        self.root = root
+        self.params = {}
+        
+        root.title('FFT-EIS Controller')
+        root.attributes('-topmost', 1)
+        root.attributes('-topmost', 0)
+        root.option_add('*tearOff', FALSE)
+        
+        
+        # Top left: Control
+        topleft     = Frame(self.root)
+        topleft.grid(row=0, column=0, sticky=(N,S))
+        
+        # Top right: input fields
+        topright    = Frame(self.root)
+        topright.grid(row=0, column=1, sticky=(N,S))
+        
+        # Bottom left: figure
+        botleft     = Frame(self.root)
+        botleft.grid(row=1, column=0, sticky=(N,S))
+        self.fig = plt.Figure(figsize=(5,4), dpi=100)
+        self.ax = self.fig.add_subplot(111)
+        self.canvas = FigureCanvasTkAgg(self.fig, master=botleft)
+        self.canvas.get_tk_widget().grid(row=0, column=0)
+
+        # Bottom right: console
+        botright    = Frame(self.root)
+        botright.grid(row=1, column=1, sticky=(N,S))
+        console = Text(botright, width=50, height=25)
+        console.grid(row=0, column=0, sticky=(N,S,E,W))
+        pl = PrintLogger(console)
+        sys.stdout = pl
+        
+        
+        
+        
+        
         
     # Spectrum/ waveform display window
     
